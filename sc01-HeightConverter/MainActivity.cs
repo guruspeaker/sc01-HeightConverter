c3d8a164fa2c78d822087f09af551f14a434e5b7
guruspeaker <32222625+guruspeaker@users.noreply.github.com>
2017-10-09 10:40:05 -0400

    Add project files.

---
+++ sc01-HeightConverter/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace sc01_HeightConverter
+{
+	[Activity(Label = "sc01_HeightConverter", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+		}
+	}
+}
+
